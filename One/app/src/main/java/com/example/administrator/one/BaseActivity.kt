3ef8495966890b0a0a7af259e1d0eea99000b4f8
88340374 <88340374@100fen.cn>
2019-03-30 19:37:17 +0800

    【Android】【权限申请】

--- One/app/src/main/java/com/example/administrator/one/BaseActivity.kt
+++ One/app/src/main/java/com/example/administrator/one/BaseActivity.kt
@@ -1,10 +1,14 @@
 package com.example.administrator.one
 
 import android.os.Bundle
+import android.view.View
 import com.gyf.barlibrary.ImmersionBar
+import com.tbruyelle.rxpermissions2.RxPermissions
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
 
 abstract class BaseActivity : RxAppCompatActivity() {
+
+    private var rxPermissions: RxPermissions? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +32,42 @@ abstract class BaseActivity : RxAppCompatActivity() {
 
     }
 
+    /**
+     * 请求多个权限挨个返回结果
+     */
+    protected fun requestPermissions(vararg permissions: String) {
+        if (rxPermissions == null) {
+            rxPermissions = RxPermissions(this)
+        }
+        rxPermissions
+                ?.requestEachCombined(*permissions)
+                ?.subscribe({
+                    when {
+                        it.granted -> permissionRequestResult(true)
+                        it.shouldShowRequestPermissionRationale -> {//不再提示 引导用户自己打开该权限
 
+                        }
+                        else -> {
+                            permissionRequestResult(false)
+                        }
+                    }
+                })
+    }
+
+    /**
+     * 请求一个或多个权限返回一个结果
+     */
+    protected fun requestPermission(vararg permission: String) {
+        if (rxPermissions == null) {
+            rxPermissions = RxPermissions(this)
+        }
+        rxPermissions
+                ?.request(*permission)
+                ?.subscribe {
+                    permissionRequestResult(it)
+                }
+    }
+
+    protected fun permissionRequestResult(granted: Boolean) {}
 
 }
